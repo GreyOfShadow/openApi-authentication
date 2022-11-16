2b480d80a867283a823ef226a2308c5a0acf5b17
lwt <532686712@qq.com>
2022-11-16 09:40:06 +0800

    init

---
+++ main/Client.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"openApi-authentication/common/auth"
+)
+
+func main() {
+
+	var config auth.AkskConfig
+	config.AccessKey = "{accessKey}"
+	config.SecretKey = "{secretKey}"
+	config.EndPoint = "{endPoint}"
+	config.Uri = "/api/test/example"
+	config.Method = "POST"
+	var requestBody string = ""
+	response := auth.Invoke(config, requestBody)
+	fmt.Printf("response body is %#v\n", response)
+}
