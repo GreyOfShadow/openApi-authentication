35d5d8a60a91385acf37f6dd927fd216d18f9134
lwt <532686712@qq.com>
2023-01-28 13:31:50 +0800

    处理错误

--- common/util/http_utils.go
+++ common/util/http_utils.go
@@ -15,20 +15,29 @@ func Call(requestMsg model.HttpRequestMsg) string {
 	req, err := http.NewRequest(requestMsg.Method, requestMsg.Url, strings.NewReader(requestMsg.Body))
 	if err != nil {
 		// handle error
+		fmt.Printf("错误Call：%v", err)
+		return ""
 	}
 	for key := range requestMsg.Headers {
 		req.Header.Set(key, requestMsg.Headers[key])
 	}
 	resp, err := client.Do(req)
+	if err != nil {
+		// handle error
+		fmt.Printf("错误ReadAll：%v", err)
+		return ""
+	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-
+			fmt.Printf("错误Calldefer：%v", err)
 		}
 	}(resp.Body)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		// handle error
+		fmt.Printf("错误ReadAll：%v", err)
+		return ""
 	}
 	fmt.Println(resp)
 	return string(body)
